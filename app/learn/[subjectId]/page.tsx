2d39f73ab9df5c97960057671fba58b7d8c414f1
Deneva <deneva2520@gmail.com>
2026-04-19 18:30:40 +0300

    changes

--- app/learn/[subjectId]/page.tsx
+++ app/learn/[subjectId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { db } from "../../../lib/firebase";
 import {
   collection,
@@ -12,13 +12,39 @@ import {
 
 export default function ModulesPage() {
 
+  const router = useRouter();
   const params = useParams();
   const subjectId = params.subjectId as string;
-  console.log("🔥 URL subjectId:", subjectId);
 
   console.log("🔥 URL subjectId:", subjectId);
 
   const [modules, setModules] = useState<any[]>([]);
+  useEffect(() => {
+
+  const q = query(
+    collection(db, "modules"),
+    where("subjectId", "==", subjectId)
+  );
+
+  const unsubscribe = onSnapshot(q, (snapshot) => {
+
+    const data: any[] = [];
+
+    snapshot.forEach((doc) => {
+      data.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    console.log("📦 modules from DB:", data);
+
+    setModules(data);
+  });
+
+  return () => unsubscribe();
+
+}, [subjectId]);
 
   useEffect(() => {
 
@@ -53,21 +79,38 @@ export default function ModulesPage() {
 
 }, [subjectId]);
 
-  return (
-    <main style={{ padding: 40 }}>
+ return (
+  <main style={{ padding: 40 }}>
+    <h1>📚 Модули</h1>
 
-      <h1>📚 Модули</h1>
-
-      {modules.length === 0 ? (
-        <p>Няма модули 😢</p>
-      ) : (
-        modules.map((m) => (
-          <div key={m.id} style={{ marginTop: 20 }}>
-            {m.name}
-          </div>
-        ))
-      )}
-
-    </main>
+    {modules.length === 0 ? (
+      <p>Няма модули 😢</p>
+    ) : (
+      modules.map((module: any) => (
+        <div
+          key={module.id}
+          onClick={() => {
+            console.log("CLICK module:", module.id);
+            router.push(`/learn/${subjectId}/${module.id}`);
+          }}
+          style={{
+            background: "#002b15",
+            border: "1px solid #00ff88",
+            borderRadius: "14px",
+            width: "220px",
+            padding: "30px",
+            cursor: "pointer",
+            marginTop: "20px",
+            color: "#FFD700",
+            textAlign: "center",
+            fontSize: "20px",
+            fontWeight: "600",
+          }}
+        >
+          {module.name}
+        </div>
+      ))
+    )}
+  </main>
   );
 }
